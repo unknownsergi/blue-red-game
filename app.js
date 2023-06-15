f341ac7d3194d45881b916fca9ab2263b961f18a
Sergi Camps <sergicampsgili@gmail.com>
2023-06-15 21:14:42 +0200

    not finished

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-const botons = document.querySelector(".botons");
-const boto = document.querySelectorAll(".boto");
+const container = document.querySelector(".boto");
+const botons = document.querySelectorAll(".boto");
 
-let color_array = [];
-console.log(random_num);
-var random_num = 0;
+let secret_colors = [];
+let buttons_colors = [];
 for (let i = 0; i < 2; i++) {
-	if (random_num === 0) {
-		color_array[i] = "blue";
+	let random_num = Math.round(Math.random());
+	if (random_num == 0) {
+		secret_colors[i] = "blue";
 	} else {
-		color_array[i] = "red";
+		secret_colors[i] = "red";
 	}
-	console.log(i);
 }
-console.log(color_array);
+console.log(secret_colors);
 
 botons.addEventListener("click", (e) => {
 	if (e.target.classList.contains("boto")) {
 		e.target.classList.toggle("blau");
+		if (e.target.classList.contains("blau")) {
+			buttons_colors[0] = "blue";
+		} else {
+			buttons_colors[0] = "red";
+		}
 	}
+	updateSomething();
 });
